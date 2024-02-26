442924d1c3938c7a47a36964a1cad597e7659c32
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-02-26 23:31:41 +0500

    Added the send message functionality and also added the brodcasting messages to connected clients functionality

--- public/main.js
+++ public/main.js
@@ -1,7 +1,32 @@
 const socket = io();
 
 const clientsTotal = document.getElementById('cleintsTotal')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('nameInput')
+const messageForm = document.getElementById('messageForm')
+const messageInput = document.getElementById('messageInput')
+
+messageForm.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  sendMessage()
+})
 
 socket.on('clientsTotal',(data)=>{
    clientsTotal.innerHTML = `Total Clients : ${data}`
+})
+
+function sendMessage(){
+   console.log(messageInput.value)
+   const data = {
+      name : nameInput.value,
+      message : messageInput.value,
+      dateTime : new Date()
+
+   }
+   socket.emit('message',data)
+
+   
+}
+socket.on('chat-message', (data)=>{
+    console.log(data)
 })
--- server.js
+++ server.js
@@ -20,4 +20,8 @@ function onConnected(socket) {
         socketsConnected.delete(socket.id)
         io.emit('clientsTotal', socketsConnected.size)
     })
+    socket.on('message', (data)=>{
+        console.log(data)
+        socket.broadcast.emit('chat-message',data)
+    })
 }
