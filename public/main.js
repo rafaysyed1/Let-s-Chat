3ab817dffa349655860ceae0f9c2c903a0118de4
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-02-27 23:55:32 +0500

    Added the add to chat message while using moment library

--- public/main.js
+++ public/main.js
@@ -6,27 +6,40 @@ const nameInput = document.getElementById('nameInput')
 const messageForm = document.getElementById('messageForm')
 const messageInput = document.getElementById('messageInput')
 
-messageForm.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  sendMessage()
+messageForm.addEventListener('submit', (e) => {
+   e.preventDefault()
+   sendMessage()
 })
 
-socket.on('clientsTotal',(data)=>{
+socket.on('clientsTotal', (data) => {
    clientsTotal.innerHTML = `Total Clients : ${data}`
 })
 
-function sendMessage(){
+function sendMessage() {
    console.log(messageInput.value)
    const data = {
-      name : nameInput.value,
-      message : messageInput.value,
-      dateTime : new Date()
+      name: nameInput.value,
+      message: messageInput.value,
+      dateTime: new Date()
 
    }
-   socket.emit('message',data)
+   socket.emit('message', data)
+   addMessagetoChat(true, data)
+   messageInput.value = ''
 
-   
+
 }
-socket.on('chat-message', (data)=>{
-    console.log(data)
+socket.on('chat-message', (data) => {
+   console.log(data)
+   addMessagetoChat(false, data)
 })
+
+function addMessagetoChat(isownerMessage, data) {
+   const messageElement = `<li class="${isownerMessage ? "messageRight" : "messageLeft"}">
+   <p class="message">${data.message}
+       <span>${data.name}. ${moment(data.dateTime).fromNow()}</span>
+   </p>
+  
+</li>`
+   messageContainer.innerHTML += messageElement
+}
